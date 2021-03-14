926894108591a1a2f16beb57708912c3773802d6
Aya berjawi <ayabarjawi@gmail.com>
2021-03-14 11:48:26 +0200

    lan

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 require('dotenv').config();
-// const pg = require('pg');
+const pg = require('pg');
 // const methodOverride = require('method-override');
 
 
 
 const cors = require('cors');
 const superagent = require('superagent');
-// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 
 
 const PORT = process.env.PORT || 3030;
@@ -19,6 +19,10 @@ server.set('view engine', 'ejs');
 
 server.use(express.static('./public'));
 server.use(express.urlencoded({extended:true}));
+
+server.get('/hello', (req,res) => {
+    res.render('./pages/index');
+})
 
 
 
